7ee227fd1b113ea69134db49c73bd4fb5036682d
Arturs Girons <arturs.girons@gmail.com>
2017-06-22 19:06:26 +0300

    no message

--- src/server/mounts/main.js
+++ src/server/mounts/main.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
           ${context.style}
         </head>
         <body>
+          <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-101492318-2', 'auto');ga('send', 'pageview');</script>
           <div id="app">${context.view}</div>
           ${context.state}
           <script src="${assets.js.vendor}"></script>
